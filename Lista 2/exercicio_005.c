1ea3c4ec968fa8ea27e612e6fd5641ba2609da49
João Pedro Schumacher <85599252+joaoschumacher@users.noreply.github.com>
2024-09-04 23:11:48 +0000

    Exercício 5

---
+++ Lista 2/exercicio_005.c
@@ -0,0 +1,28 @@
+/*
+ * Programa: exercicio_005.c
+ * Descrição: Um motorista deseja encher o tanque do seu carro com gasolina. Escreva um algoritmo para ler o preço do litro da gasolina e o valor que o motorista tem disponível para abastecer. Calcule quantos litros ele conseguiu colocar no tanque e exiba na tela.
+ * Autor: João Pedro Schumacher
+ * 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    // Declaração de variáveis
+    float precoGasolina = 0.00;
+    float valorMotorista = 0.00;
+    float litros = 0.00;
+
+    // Pede o preço da gasolina e quanto o motorista possui de dinheiro
+    printf("Qual o preço atual da gasolina: ");
+    scanf("%f", &precoGasolina);    
+    printf("Quanto dinheiro o motorista tem disponível: ");
+    scanf("%f", &valorMotorista);
+
+    // Calcula quantidade de gasolina que o motorista pode comprar
+    litros = valorMotorista / precoGasolina;
+
+    // Exibe o resultado
+    printf("\nCom R$ %.2f é possível abastecer %.2f litros de gasolina.\n", valorMotorista, litros);    
+}
