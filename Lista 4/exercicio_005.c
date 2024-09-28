472179ecfad01c4989d3d15d6770da2d012c980b
João Pedro Schumacher <85599252+joaoschumacher@users.noreply.github.com>
2024-09-28 13:53:06 +0000

    Exercício 5

--- Lista 4/exercicio_005.c
+++ Lista 4/exercicio_005.c
@@ -12,7 +12,64 @@
 
 #include <stdio.h>
 
+// Constantes
+#define MEDIA_APROVACAO 6.0
+
 int main()
 {
-    
+    // Declaração de variáveis
+    int numeroAlunos = 0;
+    float grauA = 0.00;
+    float grauB = 0.00;
+    float grauC = 0.00;
+    float mediaFinal = 0;
+    char opcaoRecuperacao;
+
+    // Aceita a quantidade de alunos
+    printf("A nota de quantos alunos será calculada? ");
+    scanf("%i", &numeroAlunos);
+
+    // Loop de quantidade de alunos que serão cadastrados
+    for (int i = 0; i < numeroAlunos; i++) {
+        printf("--------------------------------\n");
+        printf("Aluno %i:\n", i + 1);
+        printf("--------------------------------\n");
+        printf("Nota grau A: ");
+        scanf("%f", &grauA);
+        printf("Nota grau B: ");
+        scanf("%f", &grauB);
+
+        // Calcula média final
+        mediaFinal = (grauA + 2*grauB) / 3;
+
+        // Exibe resultado na tela
+        printf("\n\nSua media final e %.2f\n", mediaFinal);    
+        if(mediaFinal >= MEDIA_APROVACAO) {
+            printf("Voce passou!\n\n");
+        } else {
+            printf("Voce ficou em recuperacao! Ira fazer o grau C, deseja substituir a nota do Grau A ou B?    [A]Grau A   [B]Grau B  ");
+        // Limpa o buffer antes de ler a opção
+            getchar();  // Remove o caractere de nova linha do buffer
+            scanf("%c", &opcaoRecuperacao);
+
+            printf("\n\nNota grau C: ");
+            scanf("%f", &grauC);
+
+            if (opcaoRecuperacao == 'A'){
+                // Calcula média final substituindo Grau A
+                mediaFinal = (grauC + 2*grauB) / 3;
+            } else {
+                // Calcula média final substituindo Grau B
+                mediaFinal = (grauA + 2*grauC) / 3;
+            }
+        // Exibe resultado na tela
+            printf("\n\nSua media final e %.2f\n", mediaFinal);    
+            if(mediaFinal >= MEDIA_APROVACAO) {
+                printf("Voce passou!\n\n");
+            } else {
+                printf("Voce reprovou!\n\n");
+            }
+        }
+    }
+    printf("--------------------------------\n");
 }
