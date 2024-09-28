85f9d493a96b6db170d54be4f54274476457cbc1
João Pedro Schumacher <85599252+joaoschumacher@users.noreply.github.com>
2024-09-28 14:32:36 +0000

    Exercício 6

--- Lista 4/exercicio_006.c
+++ Lista 4/exercicio_006.c
@@ -8,8 +8,41 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    
+    // Inicializa gerador de números aleatórios
+    srand(time(0));
+
+    // Declaração de constantes
+    int numeroRandom = 0;
+    int menor = 100;
+    int maior = 0;
+    float media = 0.0;
+
+    printf("Numeros sorteados: \n");
+    // Loop para sortear os números
+    for(int i = 0; i<5; i++) {
+        numeroRandom = rand() % 100;
+        printf("%i ", numeroRandom);
+        // Verifica o menor número
+        if (numeroRandom < menor) {
+            menor = numeroRandom;
+        }
+        // Verifica o maior número
+        if (numeroRandom > maior) {
+            maior = numeroRandom;
+        }
+        // Calcula média
+        media += numeroRandom;
+    }
+    media = media / 5;
+
+    // Exibe resultados
+    printf("\nResultados:\n");
+    printf("Menor: %i\n", menor);
+    printf("Maior: %i\n", maior);
+    printf("Media: %.2f\n", media);
 }
