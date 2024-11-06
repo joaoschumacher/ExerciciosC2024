87b7fd37c807310c7312d8b35bcae162aeae8879
João Pedro Schumacher <85599252+joaoschumacher@users.noreply.github.com>
2024-11-06 20:45:46 -0300

    Exercício 1

--- Lista 6/exercicio_001.c
+++ Lista 6/exercicio_001.c
@@ -13,10 +13,35 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
+// Declaração de constantes
+const int MAX_VETOR = 5;
+const int VETORES = 3;
 
 int main()
 {
+    // Declaração de variáveis 
+    int vetor1[] = {1, 5, 9, 2, 5};
+    int vetor2[] = {7, 4, 13, 21, 6};
+    int vetor3[] = {8, -3, 5, 7, 12};
+    int matriz[VETORES][MAX_VETOR] = {};
+
+    // Loop para popular a matriz
+    for (int i = 0; i < MAX_VETOR; i++) {
+        matriz[0][i] = vetor1[i];
+        matriz[1][i] = vetor2[i];
+        matriz[2][i] = vetor3[i];
+    }
+
+    // Loop para ler dados da matriz
+    for (int i = 0; i < VETORES; i++) {
+        printf("|");
+        for (int j = 0; j < MAX_VETOR; j++) {
+            // Exibe cada valor da matriz
+            printf(" %i", matriz[i][j]);
+        }
+        printf(" |\n");
+    }
+    
     
 }
