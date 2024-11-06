1868f23846dbe5e48850949541c2eaaa6cc71957
João Pedro Schumacher <85599252+joaoschumacher@users.noreply.github.com>
2024-11-06 20:54:32 -0300

    Exercício 3

--- Lista 6/exercicio_003.c
+++ Lista 6/exercicio_003.c
@@ -13,7 +13,33 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Declaração de constantes
+const int TAMANHO_MATRIZ = 4;
+
 int main()
 {
+    // Declaração de variáveis
+    int matriz[TAMANHO_MATRIZ][TAMANHO_MATRIZ] = {};
+
+    // Loop para gerar a matriz
+    for (int i = 0; i < TAMANHO_MATRIZ; i++) {
+        for (int j = 0; j < TAMANHO_MATRIZ; j++) {
+            if(i == j) {
+                matriz[i][j] = 1;
+            } else {
+                matriz[i][j] = 0;
+            }
+        }
+        
+    }
     
+    // Loop para ler dados da matriz identidade
+    for (int i = 0; i < TAMANHO_MATRIZ; i++) {
+        printf("|");
+        for (int j = 0; j < TAMANHO_MATRIZ; j++) {
+            // Exibe cada valor da matriz 
+            printf(" %i", matriz[i][j]);
+        }
+        printf(" |\n");
+    }
 }
