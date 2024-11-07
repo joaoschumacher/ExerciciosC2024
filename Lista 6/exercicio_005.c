9ed5a146068582e5f8749091e958b24845993e45
João Pedro Schumacher <85599252+joaoschumacher@users.noreply.github.com>
2024-11-06 21:34:57 -0300

    Exercício 5

--- Lista 6/exercicio_005.c
+++ Lista 6/exercicio_005.c
@@ -10,7 +10,46 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Declaração de constantes
+const int MAX_LINHAS = 4;
+const int MAX_COLUNAS = 6;
+
+
 int main()
 {
-    
+    // Declaração de variáveis
+    int matriz[MAX_LINHAS][MAX_COLUNAS] = {};
+    int maiorValor = 0;
+    int menorValor = 0;
+
+    // Inicializa gerador de números aleatórios
+    srand(time(0));
+
+    // Loop para gerar a matriz
+    for (int i = 0; i < MAX_LINHAS; i++) {
+        for (int j = 0; j < MAX_COLUNAS; j++) {
+            matriz[i][j] = rand() % (21) - 10;
+        }
+    }
+ 
+    // Loop para ler dados da matriz
+    for (int i = 0; i < MAX_LINHAS; i++) {
+        printf("|");
+        for (int j = 0; j < MAX_COLUNAS; j++) {
+            // Exibe cada valor da matriz 
+            printf(" %i", matriz[i][j]);
+            // Testa se é o maior valor
+            if (matriz[i][j] > maiorValor) {
+                maiorValor = matriz[i][j];
+            }
+            // Testa se é o menor valor ou é a primeira comparação
+            if (matriz[i][j] < menorValor || (i == 1 && j == 1)) {
+                menorValor = matriz[i][j];
+            }
+        }
+        printf(" |\n");
+    }
+    // Exibe resultados
+    printf("Maior valor: %i\n", maiorValor);
+    printf("Menor valor: %i\n", menorValor);
 }
