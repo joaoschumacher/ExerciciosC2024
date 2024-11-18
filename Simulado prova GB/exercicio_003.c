2e6d7b0a90be9025527696b5c56626538d32e937
João Pedro Schumacher <85599252+joaoschumacher@users.noreply.github.com>
2024-11-18 02:29:05 +0000

    Simulado prova GB

---
+++ Simulado prova GB/exercicio_003.c
@@ -0,0 +1,25 @@
+/*
+ * Programa: exercicio_003.c
+ * Descrição: Implemente um programa que, dado um array de 20 inteiros preenchidos com valores entre 0 e 4, faça a
+ * contagem de quantos valores de cada um dos números possíveis este vetor contem.
+ * Exemplo: 
+ *  array: |0|3|2|2|4|4|1|0|0|0|4|4|2|3|1|2|0|2|1|3|
+ *  Retornaria a seguinte contagem:
+ *  Quantidade de valores 0: 5
+ *  Quantidade de valores 1: 3
+ *  Quantidade de valores 2: 5
+ *  Quantidade de valores 3: 3
+ *  Quantidade de valores 4: 4
+ * 
+ * Autor: João Pedro Schumacher
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+
+}
